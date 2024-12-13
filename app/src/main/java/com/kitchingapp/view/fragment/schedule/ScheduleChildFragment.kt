16efc6a853cff4cd27fa75e177859755cc36e9e9
Eddie-Pak <foalstn@naver.com>
2024-12-13 18:16:21 +0900

    child fragment attach

--- app/src/main/java/com/kitchingapp/view/fragment/schedule/ScheduleChildFragment.kt
+++ app/src/main/java/com/kitchingapp/view/fragment/schedule/ScheduleChildFragment.kt
@@ -2,8 +2,10 @@ package com.kitchingapp.view.fragment.schedule
 
 import android.os.Bundle
 import android.view.View
+import com.kitchingapp.adapter.ScheduleFixAdapter
 import com.kitchingapp.common.BaseFragment
 import com.kitchingapp.databinding.ChildfragmentScheduleDepartmentBinding
+import com.kitchingapp.domain.entities.Schedule
 
 class ScheduleChildFragment: BaseFragment<ChildfragmentScheduleDepartmentBinding>(
     ChildfragmentScheduleDepartmentBinding::inflate){
@@ -14,5 +16,18 @@ class ScheduleChildFragment: BaseFragment<ChildfragmentScheduleDepartmentBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        with(binding.confirmedScheduleRecyclerView) {
+            setRvLayout(this@with)
+
+            val adapter = ScheduleFixAdapter()
+
+            val allSchedules = RestaurantGenerator.restaurantList()
+                .flatMap { it.schedules }
+                .filter { it.isFix }
+
+            adapter.submitList(allSchedules)
+
+            this.adapter = adapter
+        }
     }
 }
