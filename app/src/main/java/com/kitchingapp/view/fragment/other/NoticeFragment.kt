a15c32c5c867cccb96f05cc030838a71be56975f
kimbangto <kimchaeyeon.525@gmail.com>
2024-12-15 00:01:18 +0900

    other - invitecode, notice

---
+++ app/src/main/java/com/kitchingapp/view/fragment/other/NoticeFragment.kt
@@ -0,0 +1,31 @@
+package com.kitchingapp.view.fragment.other
+
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.kitchingapp.R
+import com.kitchingapp.common.BaseFragment
+import com.kitchingapp.databinding.FragmentInviteCodeBinding
+import com.kitchingapp.databinding.FragmentNoticeBinding
+import com.kitchingapp.databinding.FragmentOtherBinding
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import ru.ldralighieri.corbind.view.clicks
+
+class NoticeFragment : BaseFragment<FragmentNoticeBinding>(FragmentNoticeBinding::inflate) {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        navController = findNavController()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+        }
+    }
+}
--- app/src/main/java/com/kitchingapp/view/fragment/other/OtherFragment.kt
+++ app/src/main/java/com/kitchingapp/view/fragment/other/OtherFragment.kt
@@ -2,10 +2,15 @@ package com.kitchingapp.view.fragment.other
 
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import com.kitchingapp.R
 import com.kitchingapp.common.BaseFragment
 import com.kitchingapp.databinding.FragmentOtherBinding
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import ru.ldralighieri.corbind.view.clicks
 
 class OtherFragment : BaseFragment<FragmentOtherBinding>(FragmentOtherBinding::inflate) {
     private lateinit var navController: NavController
@@ -17,5 +22,27 @@ class OtherFragment : BaseFragment<FragmentOtherBinding>(FragmentOtherBinding::i
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            inviteCodeBtn.clicks().onEach {
+                navController.navigate(R.id.action_otherFragment_to_inviteCodeFragment)
+            }.launchIn(lifecycleScope)
+
+            noticeBtn.clicks().onEach {
+                navController.navigate(R.id.action_otherFragment_to_noticeFragment)
+            }.launchIn(lifecycleScope)
+
+            departmentManagementBtn.clicks().onEach {
+                navController.navigate(R.id.action_otherFragment_to_humanResourceFragment)
+            }.launchIn(lifecycleScope)
+
+            scheduleTimeBtn.clicks().onEach {
+                navController.navigate(R.id.action_otherFragment_to_scheduleTimeFragment)
+            }.launchIn(lifecycleScope)
+
+            memberBtn.clicks().onEach {
+                navController.navigate(R.id.action_otherFragment_to_memberListFragment)
+            }.launchIn(lifecycleScope)
+        }
     }
 }
