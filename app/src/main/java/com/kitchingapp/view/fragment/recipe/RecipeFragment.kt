273ed638f0ebe01919dc9de9646d9e77abec5f09
Eddie-Pak <foalstn@naver.com>
2024-12-17 02:08:12 +0900

    feat: recipes clicks 구현

--- app/src/main/java/com/kitchingapp/view/fragment/recipe/RecipeFragment.kt
+++ app/src/main/java/com/kitchingapp/view/fragment/recipe/RecipeFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.View
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.kitchingapp.common.BaseFragment
 import com.kitchingapp.databinding.FragmentRecipeBinding
 import com.kitchingapp.R
+import com.kitchingapp.adapter.RecipeRecycleAdapter
+import com.kitchingapp.common.KitchingApplication
 
 class RecipeFragment : BaseFragment<FragmentRecipeBinding>(FragmentRecipeBinding::inflate){
     private lateinit var navController: NavController
@@ -14,10 +17,18 @@ class RecipeFragment : BaseFragment<FragmentRecipeBinding>(FragmentRecipeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         navController = findNavController()
-        childFragmentManager.beginTransaction().replace(R.id.recipeFragmentContainer, RecipeChildFragment()).commit()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val recipeDate = RestaurantGenerator.restaurantList().flatMap { it.recipes }
+
+        with(binding.recipeRV) {
+            layoutManager = GridLayoutManager(KitchingApplication.getAppContext(), 2)
+            val recipeAdapter = RecipeRecycleAdapter(viewLifecycleOwner, navController)
+            recipeAdapter.submitList(recipeDate)
+            this.adapter = recipeAdapter
+        }
     }
 }
