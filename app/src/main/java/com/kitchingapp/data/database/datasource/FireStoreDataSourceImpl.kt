f3ad454d557d6adca886010dae5b3d809763f221
kimbangto <kimchaeyeon.525@gmail.com>
2024-12-22 00:57:51 +0900

    create firestore data source impl and repository

---
+++ app/src/main/java/com/kitchingapp/data/database/datasource/FireStoreDataSourceImpl.kt
@@ -0,0 +1,68 @@
+package com.kitchingapp.data.database.datasource
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.kitchingapp.domain.entities.Schedule
+import com.kitchingapp.domain.entities.Team
+import kotlinx.coroutines.tasks.await
+
+class FireStoreDataSourceImpl(private val db: FirebaseFirestore): RemoteDataSource {
+
+    override suspend fun getTeams(userId: String): List<Team> {
+        val teams = db.collection("team").whereEqualTo("ownerId", userId).get().await()
+
+        return if(teams.isEmpty) emptyList()
+        else teams.toObjects(Team::class.java)
+    }
+
+    override suspend fun getTeamSchedules(teamId: String, date: String): List<Schedule> {
+        val schedules = db.collection("schedule").whereEqualTo("teamId", teamId).whereEqualTo("date", date).get().await()
+        return if(schedules.isEmpty) emptyList()
+        else schedules.toObjects(Schedule::class.java)
+    }
+
+    override suspend fun getScheduleTimeName(teamId: String, scheduleTimeId: String): String {
+        val scheduleTime = db.collection("scheduleTime").whereEqualTo("id", scheduleTimeId).get().await().documents.firstOrNull()
+
+        return scheduleTime?.getString("name")!!
+    }
+
+    override suspend fun getDepartmentId(teamId: String, userId: String): String {
+        val userTeam = db.collection("user-team").whereEqualTo("teamId", teamId).whereEqualTo("userId", userId).get().await().documents.firstOrNull()
+
+        return userTeam?.getString("departmentId")!!
+    }
+
+    override suspend fun getDepartmentName(teamId: String, departmentId: String): String {
+        val department = db.collection("department").whereEqualTo("id", departmentId).get().await().documents.firstOrNull()
+
+        return department?.getString("name")!!
+    }
+
+    override suspend fun getUserName(userId: String): String {
+        val user = db.collection("user").whereEqualTo("id", userId).get().await().documents.firstOrNull()
+
+        return user?.getString("userName")!!
+    }
+
+    override suspend fun createSchedule(schedule: Schedule): Boolean {
+        val scheduleMap = mapOf(
+            "id" to schedule.id,
+            "date" to schedule.date,
+            "scheduleTimeId" to schedule.scheduleTimeId,
+            "teamId" to schedule.teamId,
+            "userId" to schedule.userId,
+            "isFix" to schedule.isFix
+        )
+        val createTask = db.collection("schedule").add(scheduleMap).await().get()
+        return createTask.exception == null
+    }
+
+    override suspend fun deleteSchedule(scheduleId: String): Boolean {
+        var deleteTaskResult = false
+
+        db.collection("schedule").document(scheduleId).delete()
+            .addOnSuccessListener { deleteTaskResult = true }
+
+        return deleteTaskResult
+    }
+}
