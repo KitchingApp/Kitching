c230a4ac134b7dfe1622b851b6476a2331a6408e
Eddie-Pak <foalstn@naver.com>
2024-12-21 17:33:01 +0900

    refactor: entities model 수정

--- app/src/main/java/com/kitchingapp/domain/entities/User.kt
+++ app/src/main/java/com/kitchingapp/domain/entities/User.kt
@@ -1,10 +1,3 @@
 package com.kitchingapp.domain.entities
 
-import java.time.LocalDate
-
-data class User(
-    val name: String,
-    val birth: LocalDate,
-    val email: String,
-    val healthCertificate: LocalDate
-)
+data class User(val id: String, val userImage: String, val userName: String)
---
+++ app/src/main/java/com/kitchingapp/domain/entities/UserTeam.kt
@@ -0,0 +1,3 @@
+package com.kitchingapp.domain.entities
+
+data class UserTeam(val id : String, val departmentId: String, val staffLevelId: String, val teamId: String, val userId: String)
