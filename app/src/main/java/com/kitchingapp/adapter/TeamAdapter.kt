085c40e175373756264a47f3f4efb6024ffe2816
kimbangto <kimchaeyeon.525@gmail.com>
2024-12-25 23:35:49 +0900

    feature: team list

---
+++ app/src/main/java/com/kitchingapp/adapter/TeamAdapter.kt
@@ -0,0 +1,84 @@
+package com.kitchingapp.adapter
+
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.LifecycleCoroutineScope
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.kitchingapp.R
+import com.kitchingapp.data.database.dto.DepartmentDTO
+import com.kitchingapp.data.database.dto.TeamDTO
+import com.kitchingapp.data.database.repository.LocalRepository
+import com.kitchingapp.data.database.repository.RemoteRepository
+import com.kitchingapp.data.database.usecase.LocalType
+import com.kitchingapp.data.database.usecase.LocalTypeUseCase
+import com.kitchingapp.data.database.usecase.RemoteType
+import com.kitchingapp.data.database.usecase.RemoteTypeUseCase
+import com.kitchingapp.databinding.ItemTeamlistBinding
+import com.kitchingapp.domain.entities.Team
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import ru.ldralighieri.corbind.view.clicks
+
+class TeamAdapter(private val drawer: DrawerLayout, private val context: Context, private val lifecycleScope: LifecycleCoroutineScope, private val navController: NavController): ListAdapter<TeamDTO, TeamAdapter.TeamViewHolder>(diffUtil) {
+
+    private val localRepository: LocalRepository by lazy {
+        LocalTypeUseCase(context).selectLocalType(LocalType.DATASTORE)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TeamViewHolder {
+        val binding = ItemTeamlistBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TeamViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(
+        holder: TeamViewHolder,
+        position: Int
+    ) {
+        holder.bindTeam(currentList[position])
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<TeamDTO>() {
+            override fun areItemsTheSame(
+                oldItem: TeamDTO,
+                newItem: TeamDTO
+            ): Boolean {
+                return oldItem.teamId == newItem.teamId
+            }
+
+            override fun areContentsTheSame(
+                oldItem: TeamDTO,
+                newItem: TeamDTO
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+
+    inner class TeamViewHolder(val binding: ItemTeamlistBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bindTeam(team: TeamDTO) {
+            with(binding) {
+                teamNameTV.text = team.teamName
+                teamListCV.clicks().onEach {
+                    lifecycleScope.launch {
+                        localRepository.saveTeamId(team.teamId)
+                        drawer.closeDrawers()
+                    }
+                }.launchIn(lifecycleScope)
+            }
+        }
+    }
+}
--- app/src/main/java/com/kitchingapp/data/database/repository/FireStoreRepositoryImpl.kt
+++ app/src/main/java/com/kitchingapp/data/database/repository/FireStoreRepositoryImpl.kt
@@ -2,13 +2,29 @@ package com.kitchingapp.data.database.repository
 
 import com.kitchingapp.data.database.datasource.FireStoreDataSourceImpl
 import com.kitchingapp.data.database.dto.ScheduleDTO
-import com.kitchingapp.domain.entities.Team
+import com.kitchingapp.data.database.dto.TeamDTO
 
 class FireStoreRepositoryImpl(private val dataSource: FireStoreDataSourceImpl): RemoteRepository {
 
-    override suspend fun getTeamsByUserId(userId: String): List<Team> {
-        return dataSource.getTeams(userId)
+    override suspend fun getTeamsByUserId(userId: String): List<TeamDTO> {
+        val teamDTOList = mutableListOf<TeamDTO>()
+
+        dataSource.getTeams(userId).forEach {
+            teamDTOList.add(TeamDTO(it.id, it.teamName))
+        }
+
+        return teamDTOList
     }
+
+//    override suspend fun getDepartments(teamId: String): List<dropDownDepartmentsDTO> {
+//        val departmentDTOList = mutableListOf<dropDownDepartmentsDTO>()
+//
+//        dataSource.getDepartments(teamId).forEach {
+//            departmentDTOList.add(dropDownDepartmentsDTO(it.id, it.name))
+//        }
+//
+//        return departmentDTOList
+//    }
 
     override suspend fun getSchedules(teamId: String, date: String): List<ScheduleDTO> {
         val scheduleDTOList = mutableListOf<ScheduleDTO>()
