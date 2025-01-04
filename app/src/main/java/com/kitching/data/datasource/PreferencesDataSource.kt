2231a52fc4c86d86ecb79351caa73347eb51fb26
Eddie-Pak <foalstn@naver.com>
2025-01-05 02:38:02 +0900

    feat: userId get, save 구현

--- app/src/main/java/com/kitching/data/datasource/PreferencesDataSource.kt
+++ app/src/main/java/com/kitching/data/datasource/PreferencesDataSource.kt
@@ -1,27 +1,27 @@
 package com.kitching.data.datasource
 
 import android.content.Context
-import android.util.Log
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
-import kotlinx.coroutines.flow.collectLatest
+import com.kitching.common.dataStore
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
-
-val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "team")
 
 class PreferencesDataSource(private val context: Context) {
 
     companion object {
         private val TEAM_ID = stringPreferencesKey("team_id")
+        private val USER_ID = stringPreferencesKey("user_id")
     }
 
     suspend fun saveTeamId(teamId: String) {
-        context.dataStore.edit { team -> team[TEAM_ID] = teamId }
+        context.dataStore.edit { preferences -> preferences[TEAM_ID] = teamId }
     }
 
-   suspend fun getTeamId() = context.dataStore.data.first()[TEAM_ID]
+    suspend fun getTeamId(): String? = context.dataStore.data.first()[TEAM_ID]
+
+    suspend fun saveUserId(userId: String) {
+        context.dataStore.edit { preferences -> preferences[USER_ID] = userId }
+    }
+
+    suspend fun getUserId(): String? = context.dataStore.data.first()[USER_ID]
 }
