13986d85dc306f431ca5cc49893b5df173efc516
kimbangto <kimchaeyeon.525@gmail.com>
2025-01-08 22:21:19 +0900

    feature - firebaseResultHandler를 Fragment, DialogFragment의 확장함수로 변경

--- app/src/main/java/com/kitching/common/ExtensionFunctions.kt
+++ app/src/main/java/com/kitching/common/ExtensionFunctions.kt
@@ -1,7 +1,10 @@
 package com.kitching.common
 
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.kitching.common.util.ProgressDialog
 import com.kitching.data.firebase.FirebaseResult
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -16,6 +19,44 @@ fun <T> ViewModel.firebaseFlowHandler(
         stateFlow.value = FirebaseResult.Loading
         fetch().collectLatest {
             stateFlow.value = it
+        }
+    }
+}
+
+fun <T>Fragment.firebaseResultHandler(
+    firebaseResult: FirebaseResult<T>,
+    onSuccess: (T) -> Unit
+) {
+    when (firebaseResult) {
+        is FirebaseResult.Success -> {
+            onSuccess(firebaseResult.data)
+            ProgressDialog.cancel()
+        }
+        is FirebaseResult.Failure -> {
+            ProgressDialog.cancel()
+            commonToast("오류로 인해 데이터를 가져오지 못했습니다. : ${firebaseResult.throwable}")
+        }
+        else -> {
+            ProgressDialog.show(requireContext())
+        }
+    }
+}
+
+fun <T>DialogFragment.firebaseResultHandler(
+    firebaseResult: FirebaseResult<T>,
+    onSuccess: (T) -> Unit
+) {
+    when (firebaseResult) {
+        is FirebaseResult.Success -> {
+            onSuccess(firebaseResult.data)
+            ProgressDialog.cancel()
+        }
+        is FirebaseResult.Failure -> {
+            ProgressDialog.cancel()
+            commonToast("오류로 인해 데이터를 가져오지 못했습니다. : ${firebaseResult.throwable}")
+        }
+        else -> {
+            ProgressDialog.show(requireContext())
         }
     }
 }
