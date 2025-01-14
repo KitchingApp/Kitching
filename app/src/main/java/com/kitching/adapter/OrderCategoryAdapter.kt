d559e5c386a9d64245548bea6aa4a8a6e92ff5d8
Eddie-Pak <foalstn@naver.com>
2025-01-15 02:42:19 +0900

    feat: orderFragment dialog 연결

--- app/src/main/java/com/kitching/adapter/OrderCategoryAdapter.kt
+++ app/src/main/java/com/kitching/adapter/OrderCategoryAdapter.kt
@@ -1,27 +1,35 @@
 package com.kitching.adapter
 
+import android.content.Context
 import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.kitching.common.util.throttleFirst
+import com.kitching.common.util.throttleClicks
 import com.kitching.data.dto.OrderCategoryDTO
 import com.kitching.databinding.ItemBigCategoryBinding
 import com.kitching.view.fragment.order.OrderFragmentDirections
+import com.kitching.R
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import ru.ldralighieri.corbind.view.clicks
+import ru.ldralighieri.corbind.widget.itemClicks
 
 class OrderCategoryAdapter(private val lifecycleOwner: LifecycleOwner, private val navController: NavController): ListAdapter<OrderCategoryDTO, OrderCategoryAdapter.OrderViewHolder>(diffUtil) {
+
+    private lateinit var context: Context
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): OrderViewHolder {
+        context = parent.context
         val binding = ItemBigCategoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return OrderViewHolder(binding)
     }
@@ -57,11 +65,44 @@ class OrderCategoryAdapter(private val lifecycleOwner: LifecycleOwner, private v
             with(binding) {
                 categoryNameTV.text = orderCategory.categoryName
                 categoryCV.setCardBackgroundColor(Color.parseColor(orderCategory.color))
-                categoryCV.clicks().throttleFirst().onEach {
-                    val argsAction = OrderFragmentDirections.actionOrderFragmentToOrderListFragment(orderCategory.categoryId)
-                    navController.navigate(argsAction)
-                }.launchIn(lifecycleOwner.lifecycleScope)
+
+                categoryCV.throttleClicks(lifecycleOwner) {
+                    navigateToOrderList(orderCategory.categoryId)
+                }
+
+                optionBtn.throttleClicks(lifecycleOwner) {
+                    shoPopupMenu(optionBtn, orderCategory)
+                }
             }
         }
+    }
+
+    private fun navigateToOrderList(categoryId: String) {
+        val action = OrderFragmentDirections.actionOrderFragmentToOrderListFragment(categoryId)
+        navController.navigate(action)
+    }
+
+    private fun shoPopupMenu(anchor: View, orderCategory: OrderCategoryDTO) {
+        val popup = PopupMenu(context, anchor)
+        popup.menuInflater.inflate(R.menu.option_menu, popup.menu)
+
+        popup.itemClicks().onEach {
+            when(it.itemId) {
+                R.id.updateInOptionMenu -> navigateToUpdateDialog(orderCategory)
+                R.id.deleteInOptionMenu -> navigateToDeleteDialog(orderCategory)
+            }
+        }.launchIn(lifecycleOwner.lifecycleScope)
+
+        popup.show()
+    }
+
+    private fun navigateToUpdateDialog(orderCategory: OrderCategoryDTO) {
+        val action = OrderFragmentDirections.actionOrderFragmentToOrderCategoryUpdateDialog(orderCategory.categoryId, orderCategory.categoryName, orderCategory.color)
+        navController.navigate(action)
+    }
+
+    private fun navigateToDeleteDialog(orderCategory: OrderCategoryDTO) {
+        val action = OrderFragmentDirections.actionOrderFragmentToOrderCategoryDeleteDialog(orderCategory.categoryId)
+        navController.navigate(action)
     }
 }
