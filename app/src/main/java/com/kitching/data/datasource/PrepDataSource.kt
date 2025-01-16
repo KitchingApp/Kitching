d36016526a5029c0f315e182afda9bde35bff8d4
kimbangto <kimchaeyeon.525@gmail.com>
2025-01-16 18:19:50 +0900

    데이터소스 추상화 작업

---
+++ app/src/main/java/com/kitching/data/datasource/PrepDataSource.kt
@@ -0,0 +1,13 @@
+package com.kitching.data.datasource
+
+import com.kitching.domain.entities.PrepCategory
+
+interface PrepDataSource {
+    fun create() {}
+
+    fun read() {}
+
+    fun update() {}
+
+    fun delete() {}
+}
---
+++ app/src/main/java/com/kitching/data/datasource/PrepDataSourceImpl.kt
@@ -0,0 +1,22 @@
+package com.kitching.data.datasource
+
+import com.kitching.common.COLLECTION_PREP_CATEGORY
+import com.kitching.domain.entities.PrepCategory
+import kotlinx.coroutines.tasks.await
+
+class PrepDataSourceImpl: PrepDataSource {
+    override fun create() {
+        super.create()
+    }
+
+    override fun read() {
+    }
+
+    override fun update() {
+        super.update()
+    }
+
+    override fun delete() {
+        super.delete()
+    }
+}
