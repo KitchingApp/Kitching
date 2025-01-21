1dfad1292e9494220c51123bd35bbb315f841ba3
kimbangto <kimchaeyeon.525@gmail.com>
2025-01-21 23:12:20 +0900

    teamUserTeamJoin 데이터소스, 레포지토리 - 인터페이스, 구현체

---
+++ app/src/main/java/com/kitching/domain/datasource/TeamUserTeamJoinDataSource.kt
@@ -0,0 +1,7 @@
+package com.kitching.domain.datasource
+
+import com.kitching.domain.entities.Team
+
+interface TeamUserTeamJoinDataSource {
+    suspend fun getTeams(userId: String): Result<List<Team>>
+}
