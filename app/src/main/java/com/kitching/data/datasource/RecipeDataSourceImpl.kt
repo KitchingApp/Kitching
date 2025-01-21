6fa319b3ba4bd651293e93fb810024f87ad14ecb
kimbangto <kimchaeyeon.525@gmail.com>
2025-01-21 23:10:56 +0900

    recipe 데이터소스, 레포지토리 - 인터페이스, 구현체

--- app/src/main/java/com/kitching/data/datasource/RecipeDataSourceImpl.kt
+++ app/src/main/java/com/kitching/data/datasource/RecipeDataSourceImpl.kt
@@ -2,46 +2,42 @@ package com.kitching.data.datasource
 
 import android.net.Uri
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObject
 import com.google.firebase.storage.FirebaseStorage
+import com.kitching.common.COLLECTION_INGREDIENT
 import com.kitching.common.COLLECTION_RECIPE
 import com.kitching.domain.datasource.RecipeDataSource
 import com.kitching.domain.entities.Ingredient
 import com.kitching.domain.entities.Recipe
 import kotlinx.coroutines.tasks.await
 
-class RecipeDataSourceImpl(private val db: FirebaseFirestore = FirebaseFirestore.getInstance()): RecipeDataSource {
-    override suspend fun getRecipeList(teamId: String): MutableList<Recipe> {
-        val recipeListSnapshot = db.collection(COLLECTION_RECIPE)
-            .whereEqualTo("teamId", teamId)
-            .get()
-            .await()
-
-        val recipes = mutableListOf<Recipe>()
-
-        for (document in recipeListSnapshot.documents) {
-            // 기본 Recipe 필드 가져오기
-            val recipe = document.toObject(Recipe::class.java) ?: continue
-
-            // 하위 컬렉션 'ingredient' 가져오기
-            val ingredientSnapshot = document.reference.collection("ingredient").get().await()
-            val ingredients = ingredientSnapshot.toObjects(Ingredient::class.java)
-
-            // Recipe에 하위 컬렉션 데이터를 추가
-            recipes.add(
-                recipe.copy(ingredients = ingredients) // 데이터 클래스 복사로 값 설정
-            )
-        }
-        return recipes
-    }
-
-    // 파이어베이스 스토리지
+class RecipeDataSourceImpl(
+    private val db: FirebaseFirestore = FirebaseFirestore.getInstance(),
     private val storage: FirebaseStorage = FirebaseStorage.getInstance()
+): RecipeDataSource {
+    override suspend fun getRecipeList(teamId: String): Result<List<Recipe>> {
+        return runCatching {
+            db.collection(COLLECTION_RECIPE).get()
+                .await().documents.mapNotNull { documentSnapshot ->
+                val recipe = documentSnapshot.toObject(Recipe::class.java) ?: return@mapNotNull null
+                val ingredients =
+                    documentSnapshot.reference.collection(COLLECTION_INGREDIENT).get().await()
+                        .toObjects(Ingredient::class.java)
+
+                recipe.copy(ingredients = ingredients)
+            }
+        }
+    }
 
     override suspend fun uploadImageToStorage(
         imageUri: Uri,
         imageName: String,
-    ): String {
-        TODO("Not yet implemented")
+    ): Result<String> {
+        return runCatching {
+            val storageRef = storage.reference.child("recipeImage/$imageName")
+            storageRef.putFile(imageUri).await()
+            storageRef.downloadUrl.await().toString()
+        }
     }
 
     override suspend fun saveRecipe(
@@ -49,15 +45,42 @@ class RecipeDataSourceImpl(private val db: FirebaseFirestore = FirebaseFirestore
         picture: String,
         steps: List<String>,
         teamId: String,
-    ): String {
-        TODO("Not yet implemented")
+    ): Result<String> {
+        return runCatching {
+            val recipeData = mapOf(
+                "id" to "",
+                "name" to name,
+                "picture" to picture,
+                "steps" to steps,
+                "teamId" to teamId
+            )
+            val recipeDocument = db.collection("recipe").add(recipeData).await()
+            db.collection("recipe").document(recipeDocument.id).update("id", recipeDocument.id).await()
+            recipeDocument.id
+        }
     }
 
     override suspend fun saveIngredients(
         recipeId: String,
         ingredients: List<Map<String, String>>,
     ): Boolean {
-        TODO("Not yet implemented")
+        return runCatching {
+            val ingredientCollection = db.collection("recipe").document(recipeId).collection("ingredient")
+            ingredients.forEach { ingredient ->
+                // MutableMap<String, Any>로 변환
+                val ingredientData = ingredient.mapValues { (key, value) ->
+                    when (key) {
+                        "once", "twice" -> value.toIntOrNull() ?: 0
+                        else -> value
+                    }
+                }
+
+                // Firestore에 데이터 추가 및 업데이트
+                val ingredientDocument = ingredientCollection.add(ingredientData).await()
+                ingredientCollection.document(ingredientDocument.id)
+                    .update("id", ingredientDocument.id).await()
+            }
+        }.isSuccess
     }
 
 }
--- app/src/main/java/com/kitching/domain/datasource/RecipeDataSource.kt
+++ app/src/main/java/com/kitching/domain/datasource/RecipeDataSource.kt
@@ -4,18 +4,18 @@ import android.net.Uri
 import com.kitching.domain.entities.Recipe
 
 interface RecipeDataSource {
-    suspend fun getRecipeList(teamId: String): MutableList<Recipe>
+    suspend fun getRecipeList(teamId: String): Result<List<Recipe>>
 
-    suspend fun uploadImageToStorage(imageUri: Uri, imageName: String): String
+    /** return: imageUrl */
+    suspend fun uploadImageToStorage(imageUri: Uri, imageName: String): Result<String>
 
+    /** return: recipeId */
     suspend fun saveRecipe(
         name: String,
         picture: String,
         steps: List<String>,
         teamId: String
-    ): String
+    ): Result<String>
 
     suspend fun saveIngredients(recipeId: String, ingredients: List<Map<String, String>>): Boolean
-
-
 }
